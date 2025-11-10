19bfc1d48d504f36ea54e2ae981b900dd6390ee1
José Álvarez <josemejia61976751@gmail.com>
2025-11-10 06:49:59 -0600

    feat: basic styles added, from h1 to h4

--- ShadcniOS/Sources/Extensions/SHDTextStyle+Styles.swift
+++ ShadcniOS/Sources/Extensions/SHDTextStyle+Styles.swift
@@ -16,4 +16,37 @@ internal extension SHDTextStyle {
             letterSpacing: -1.2
         )
     }
+
+    static var h2: SHDTextStyle {
+        SHDTextStyle(
+            relativeFont: .title,
+            font: .variable,
+            weight: .semiBold,
+            size: 30,
+            lineHeight: 36,
+            letterSpacing: -0.75
+        )
+    }
+
+    static var h3: SHDTextStyle {
+        SHDTextStyle(
+            relativeFont: .title2,
+            font: .variable,
+            weight: .semiBold,
+            size: 24,
+            lineHeight: 32,
+            letterSpacing: -0.6
+        )
+    }
+
+    static var h4: SHDTextStyle {
+        SHDTextStyle(
+            relativeFont: .title3,
+            font: .variable,
+            weight: .semiBold,
+            size: 20,
+            lineHeight: 28,
+            letterSpacing: -0.5
+        )
+    }
 }
