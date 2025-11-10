7ec862733cdb91d1cafa3fbc1bdab3cf3cd8d7a0
José Álvarez <josemejia61976751@gmail.com>
2025-11-10 06:49:59 -0600

    feat: basic styles added, from h1 to h4

--- ShadcniOS/Sources/dummyView.swift
+++ ShadcniOS/Sources/dummyView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 internal struct DummyView: View {
     var body: some View {
-        Text("Hello world!")
+        Text("Taxing Laughter: The Joke Tax Chronicles")
             .textStyle(.h1)
+        Divider()
+        Text("The peope of the kingdom")
+            .textStyle(.h2)
+        Divider()
+        Text("The Joke Tax")
+            .textStyle(.h3)
+        Divider()
+        Text("People stopped telling jokes")
+            .textStyle(.h4)
     }
 }
 
