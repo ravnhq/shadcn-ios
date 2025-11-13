91b4e103f357f50d63632f32e43638efee818904
José Álvarez <josemejia61976751@gmail.com>
2025-11-13 07:32:12 -0600

    feat: Accordion linked with the demo project

--- ShadcniOSDemo/ShadcniOSDemo/ContentView.swift
+++ ShadcniOSDemo/ShadcniOSDemo/ContentView.swift
@@ -5,20 +5,60 @@
 //  Created by JoseAlvarez on 10/28/25.
 //
 
-import SwiftUI
 import ShadcniOS
+import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
             NavigationStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                List {
+                    NavigationLink(destination: AccordionView()) {
+                        HStack {
+                            Image(systemName: "list.bullet.rectangle.portrait")
+                            Text("Accordion")
+                        }
+                    }
+                }
+                .navigationTitle(Text("ShadcniOS Demo"))
+                .listStyle(SidebarListStyle())
             }
+            .redacted(reason: .invalidated)
         }
-        .padding()
+    }
+}
+
+struct dummyModel: SHDAccordionRepresentable {
+    var title: String
+    var content: String
+}
+
+struct AccordionView: View {
+    let dummyData: [dummyModel] = [
+        dummyModel(
+            title: "Hello from Demo",
+            content: "Expanded content from demo"
+        ),
+        dummyModel(
+            title: "Hello from Demo 2",
+            content: "Expanded content from demo 2"
+        ),
+        dummyModel(
+            title: "Hello from Demo 3",
+            content: "Expanded content from demo 3"
+        ),
+    ]
+
+    var body: some View {
+        SHDAccordion(items: dummyData)
+            .padding(.horizontal, 20)
+            .accordionStyle(size: .sm)
+        SHDAccordion(items: dummyData)
+            .padding(.horizontal, 20)
+            .accordionStyle(size: .md)
+        SHDAccordion(items: dummyData)
+            .padding(.horizontal, 20)
+            .accordionStyle(size: .lg)
     }
 }
 
