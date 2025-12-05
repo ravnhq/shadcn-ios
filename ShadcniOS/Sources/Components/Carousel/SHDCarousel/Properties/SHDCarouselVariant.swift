d883df30ddabae51dd1da3c5161af029eeb0ecc4
José Álvarez <josemejia61976751@gmail.com>
2025-12-05 11:54:24 -0600

    feat: basic properties for carousel added

---
+++ ShadcniOS/Sources/Components/Carousel/SHDCarousel/Properties/SHDCarouselVariant.swift
@@ -0,0 +1,36 @@
+//
+//  SHDCarouselVariant.swift
+//  ShadcniOS
+//
+//  Created by JoseAlvarez on 12/1/25.
+//
+
+import Foundation
+
+enum SHDCarouselLayoutVariant {
+    case groupHorizonal
+    case singleHorizonal
+    case groupVertical
+}
+
+enum SHDCarouselProprotionVariant {
+    case oneToOne
+    case threeToFour
+    case sixteenToNine
+
+    var width: CGFloat {
+        switch self {
+        case .oneToOne: 212
+        case .threeToFour: 159
+        case .sixteenToNine: 377
+        }
+    }
+    
+    var height: CGFloat {
+        switch self {
+        case .oneToOne: 212
+        case .threeToFour: 212
+        case .sixteenToNine: 212
+        }
+    }
+}
