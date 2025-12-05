3f3bb04413152ecb0a105567f645010c08b7d4e2
José Álvarez <josemejia61976751@gmail.com>
2025-12-05 11:52:55 -0600

    feat: basic carousel component added

---
+++ ShadcniOS/Sources/Components/Carousel/SHDCarousel/SHDCarousel.swift
@@ -0,0 +1,80 @@
+//
+//  SHDCarousel.swift
+//  ShadcniOS
+//
+//  Created by JoseAlvarez on 12/1/25.
+//
+
+import SwiftUI
+
+struct SHDCarousel<Item, Content: View>: View {
+
+    @State private var index = 0
+
+    private var items: [Item]
+    private var content: (Item) -> Content
+
+    private var layoutVariant: SHDCarouselLayoutVariant = .groupHorizonal
+    private var proportionVariant: SHDCarouselProprotionVariant = .oneToOne
+
+    private var maxVisible: Int {
+        layoutVariant == .singleHorizonal ? 1 : items.count
+    }
+
+    private var visibleItems: [Item] {
+        let end = min(index + maxVisible, items.count)
+        return Array(items[index..<end])
+    }
+
+    private var carouselContent: some View {
+        SHDCarouselItem(
+            layoutVariant: layoutVariant,
+            proporcionVariant: proportionVariant,
+            visibleItems: visibleItems,
+            content: content
+        )
+    }
+
+    init(
+        items: [Item],
+        @ViewBuilder content: @escaping (Item) -> Content
+    ) {
+        self.items = items
+        self.content = content
+    }
+
+    var body: some View {
+        Group {
+            switch layoutVariant {
+            case .groupHorizonal, .singleHorizonal:
+                ScrollView(.horizontal) {
+                    HStack(spacing: .md) {
+                        carouselContent
+                    }
+                    .padding(.vertical, .xxs)
+                    .padding(.horizontal, .md)
+                }
+
+            case .groupVertical:
+                ScrollView {
+                    VStack {
+                        carouselContent
+                    }
+                    .padding(.vertical, .xxs)
+                    .padding(.horizontal, .md)
+
+                }
+            }
+        }
+        .backgroundColor(.black)
+
+    }
+
+    func carouselVariant(_ layoutVariant: SHDCarouselLayoutVariant) -> Self {
+        mutating(keyPath: \.layoutVariant, value: layoutVariant)
+    }
+}
+
+#Preview {
+    SHDCarouselPreview()
+}
