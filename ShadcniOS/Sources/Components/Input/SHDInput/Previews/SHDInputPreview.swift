c7d9a0ce428a3c28276209f38f048e92a3565ed8
Diego Matsubara <diego@Diego-Matsubara-Ravn-H6Q7593F21.local>
2025-12-08 10:28:37 -0400

    feature: Creating branch and files

---
+++ ShadcniOS/Sources/Components/Input/SHDInput/Previews/SHDInputPreview.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ShadcniOS
+//
+//  Created by Diego Takaki on 12/8/25.
+//
+
+import SwiftUI
+
+struct SHDInputPreview: View {
+    var body: some View {
+        Text("Preview here")
+    }
+}
+
+#Preview {
+    SHDInputPreview()
+}
---
+++ ShadcniOS/Sources/Components/Input/SHDInput/SHDInput.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ShadcniOS
+//
+//  Created by Diego Takaki on 12/8/25.
+//
+
+import SwiftUI
+
+struct SHDInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SHDInputPreview()
+}
