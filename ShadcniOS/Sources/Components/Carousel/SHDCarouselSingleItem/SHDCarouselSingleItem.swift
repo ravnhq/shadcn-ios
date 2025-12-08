9b481e1e8bbfb4a4f45a663a9c952ba7c76ec175
José Álvarez <josemejia61976751@gmail.com>
2025-12-08 12:34:49 -0600

    fix: the pagination for 16:9 group horizontal fixed, now is using tab view

--- ShadcniOS/Sources/Components/Carousel/SHDCarouselSingleItem/SHDCarouselSingleItem.swift
+++ ShadcniOS/Sources/Components/Carousel/SHDCarouselSingleItem/SHDCarouselSingleItem.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct SHDCarouselSingleItem<Item, Content: View>: View {
+struct SHDCarouselTabView<Item, Content: View>: View {
 
     var layoutVariant: SHDCarouselLayoutVariant
     var proportionVariant: SHDCarouselProportionVariant
     var items: [Item]
     var content: (Item) -> Content
 
-    @State private var selection = 0
+    @State private var currentPage = 0
 
     var body: some View {
         VStack(spacing: .sm) {
-            TabView(selection: $selection) {
+            TabView(selection: $currentPage) {
                 ForEach(Array(items.enumerated()), id: \.offset) { index, item in
                     content(item)
                         .horizontalFrameVariantion(
@@ -34,7 +34,9 @@ struct SHDCarouselSingleItem<Item, Content: View>: View {
             HStack(spacing: 8) {
                 ForEach(0..<items.count, id: \.self) { idx in
                     Circle()
-                        .fill(idx == selection ? SHDColor.foregroundDefault.color : SHDColor.foregroundDefault.color.opacity(0.3))
+                        .fill(idx == currentPage
+                              ? SHDColor.foregroundDefault.color
+                              : SHDColor.foregroundDefault.color.opacity(0.3))
                         .frame(width: 8, height: 8)
                 }
             }
