17a21635ab495a718e4f38b27e9d8cd90ba51d7c
José Álvarez <josemejia61976751@gmail.com>
2025-12-10 21:24:48 -0600

    fix: scroll inidicator removed, & scroll view applied in demo app

--- ShadcniOSDemo/ShadcniOSDemo/Components/Carousel/SHDCarouselDemoView.swift
+++ ShadcniOSDemo/ShadcniOSDemo/Components/Carousel/SHDCarouselDemoView.swift
@@ -29,89 +29,91 @@ struct SHDCarouselDemoView: View {
     @State private var selectedType: DataType = .text
     
     var body: some View {
-        VStack(spacing: 20) {
-            
-            Picker("Data types", selection: $selectedType) {
-                ForEach(DataType.allCases) { type in
-                    Text(type.rawValue).tag(type)
+        ScrollView {
+            VStack(spacing: 20) {
+                
+                Picker("Data types", selection: $selectedType) {
+                    ForEach(DataType.allCases) { type in
+                        Text(type.rawValue).tag(type)
+                    }
                 }
-            }
-            .pickerStyle(.segmented)
-            .padding()
-            
-            HStack {
-                Text("Layout Variant")
-
-                Spacer()
-
-                Picker("Layout Variant", selection: $layoutVariant) {
-                    Text("Group Horizontal").tag(SHDCarouselLayoutVariant.groupHorizonal)
-                    Text("Single Horizontal").tag(SHDCarouselLayoutVariant.singleHorizonal)
-                    Text("Group Vertical").tag(SHDCarouselLayoutVariant.groupVertical)
-                }
-            }
-            .padding()
-            
-            HStack {
-                Text("Proportion Variant")
-
-                Spacer()
-
-                Picker("Layout Variant", selection: $proportionVariant) {
-                    Text("1:1").tag(SHDCarouselProportionVariant.oneToOne)
-                    Text("3:4").tag(SHDCarouselProportionVariant.threeToFour)
-                    Text("16:9").tag(SHDCarouselProportionVariant.sixteenToNine)
-                }
-            }
-            .padding()
-
-            Group {
-                switch selectedType {
+                .pickerStyle(.segmented)
+                .padding()
+                
+                HStack {
+                    Text("Layout Variant")
                     
-                case .text:
-                    SHDCarousel(items: textItems) { item in
-                        HStack {
-                            Text(item)
-                                .font(.title2)
-                                .padding()
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(.secondary)
+                    Spacer()
+                    
+                    Picker("Layout Variant", selection: $layoutVariant) {
+                        Text("Group Horizontal").tag(SHDCarouselLayoutVariant.groupHorizonal)
+                        Text("Single Horizontal").tag(SHDCarouselLayoutVariant.singleHorizonal)
+                        Text("Group Vertical").tag(SHDCarouselLayoutVariant.groupVertical)
                     }
-                    .carouselLayoutVariant(layoutVariant)
-                    .carouselProportionVariant(proportionVariant)
-
-                case .numbers:
-                    SHDCarousel(items: numberItems) { item in
-                        HStack {
-                            Text("\(item)")
-                                .font(.system(size: 40, weight: .bold))
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(.secondary)
-
+                }
+                .padding()
+                
+                HStack {
+                    Text("Proportion Variant")
+                    
+                    Spacer()
+                    
+                    Picker("Layout Variant", selection: $proportionVariant) {
+                        Text("1:1").tag(SHDCarouselProportionVariant.oneToOne)
+                        Text("3:4").tag(SHDCarouselProportionVariant.threeToFour)
+                        Text("16:9").tag(SHDCarouselProportionVariant.sixteenToNine)
                     }
-                    .carouselLayoutVariant(layoutVariant)
-                    .carouselProportionVariant(proportionVariant)
-
-                case .images:
-                    SHDCarousel(items: imageItems) { item in
-                        HStack {
-                            Image(systemName: item)
-                                .resizable()
-                                .scaledToFit()
-                                .padding()
+                }
+                .padding()
+                
+                Group {
+                    switch selectedType {
+                        
+                    case .text:
+                        SHDCarousel(items: textItems) { item in
+                            HStack {
+                                Text(item)
+                                    .font(.title2)
+                                    .padding()
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.secondary)
                         }
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(.secondary)
+                        .carouselLayoutVariant(layoutVariant)
+                        .carouselProportionVariant(proportionVariant)
+                        
+                    case .numbers:
+                        SHDCarousel(items: numberItems) { item in
+                            HStack {
+                                Text("\(item)")
+                                    .font(.system(size: 40, weight: .bold))
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.secondary)
+                            
+                        }
+                        .carouselLayoutVariant(layoutVariant)
+                        .carouselProportionVariant(proportionVariant)
+                        
+                    case .images:
+                        SHDCarousel(items: imageItems) { item in
+                            HStack {
+                                Image(systemName: item)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding()
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(.secondary)
+                        }
+                        .carouselLayoutVariant(layoutVariant)
+                        .carouselProportionVariant(proportionVariant)
                     }
-                    .carouselLayoutVariant(layoutVariant)
-                    .carouselProportionVariant(proportionVariant)
                 }
             }
+            .navigationTitle("SHDCarousel")
+            .padding(.top)
         }
-        .navigationTitle("SHDCarousel")
-        .padding(.top)
     }
 }
 
