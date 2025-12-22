633527f45666e75903191ab7365e1889650b3810
Diego Matsubara <diego@Diego-Matsubara-Ravn-H6Q7593F21.local>
2025-12-22 17:19:36 -0400

    refactor: Changing naming to a better one

--- ShadcniOSDemo/ShadcniOSDemo/Components/Sonner/SHDSonnerDemoView.swift
+++ ShadcniOSDemo/ShadcniOSDemo/Components/Sonner/SHDSonnerDemoView.swift
@@ -82,7 +82,7 @@ struct SHDSonnerDemoView: View {
             Text("Text to be behind the sonner")
                 .padding(.bottom, 16)
         }
-        .showSonner(isPresented: $isPresented, position: position) {
+        .showSonner(isPresented: $isPresented, from: position) {
             SHDSonner(title: "Event has been created",
                       subtitle: "Sunday, December 03, 2023 at 9:00 AM",)
             .sonnerVariant(variant: selectedVariant, size: selectedSize)
